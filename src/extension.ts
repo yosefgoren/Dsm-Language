261d79b4830422453293730415801a296060fe61
yosef <yosefisraelgoren@gmail.com>
2024-01-21 23:45:30 +0200

    added offline support

--- src/extension.ts
+++ src/extension.ts
@@ -1,5 +1,10 @@
 import * as vscode from 'vscode';
 
+
+function getMediaPath(filename: string, context: vscode.ExtensionContext, panel: vscode.WebviewPanel) {
+	return panel.webview.asWebviewUri(vscode.Uri.joinPath(context.extensionUri, 'media', filename));
+}
+
 export function activate(context: vscode.ExtensionContext) {
 	let disposable = vscode.commands.registerCommand('desmos-calculator-window.create-desmos-window', () => {
 		let panel = vscode.window.createWebviewPanel(
@@ -9,32 +14,66 @@ export function activate(context: vscode.ExtensionContext) {
 			{enableScripts: true}
 		);
 
+
 		panel.webview.html = `
-			<!DOCTYPE html>
-			<html lang="en">
+		<!DOCTYPE html>
+		<html>
 			<head>
-				<style>
-					#cont {
-						width: 100%; /* Make the container div take up the full width of the parent element */
-						height: 100vh; /* Make the container div take up the full height of the viewport */
-					}
-					#calculator {
-						width: 100%; /* Make the calculator div take up the full width of the container */
-						height: 100%; /* Make the calculator div take up the full height of the container */
-					}
-				</style>
+				<title>Untitled</title>
+				<meta charset="utf-8">
+				<meta name="viewport" content="width=device-width, initial-scale=1">
+				<!-- <script src="https://www.desmos.com/api/v1.4/calculator.js?apiKey=dcb31709b452b1cf9dc26972add0fda6"></script> -->
+				<script type="text/javascript" src="${getMediaPath("calculator.js", context, panel)}"></script>
 			</head>
-			<body>
-				<script src="https://www.desmos.com/api/v1.8/calculator.js?apiKey=dcb31709b452b1cf9dc26972add0fda6"></script>
-				<div id="cont">
-					<div id="calculator"></div>
+			<body style="margin: 0;">
+				
+				<div style="line-height: 46px;">
+					<input id="file" type="file" accept=".json" style="display: none;"/>
+					<button id="load">load</button>
+					<button id="save">save</button>
+					<input id="title" type="text" value="Untitled">
 				</div>
-				<script>
-					var elt = document.getElementById('calculator');
-					var calculator = Desmos.GraphingCalculator(elt);
+				<div id="calculator" style="position: absolute; top: 46px; bottom: 0; left: 0; right: 0;"></div>
+				<script>            
+					window.Calc = Desmos.GraphingCalculator(document.querySelector('#calculator'));
+					window.Calc.settingsMenu.reverseContrast = true;
+					
+					function loadDesmosState(jsonString){
+						var state = JSON.parse(jsonString);
+						window.Calc.setState(state);
+					};
+					
+					document.querySelector('#file').onchange = function(e){
+						let file = e.target.files[0];
+						name = file.name.replace(/\.(json|JSON)$/, '');
+						document.querySelector('#title').value = name;
+						document.querySelector('head>title').text = name;
+						let reader = new FileReader();
+						reader.onload = function(e){
+							loadDesmosState(e.target.result);
+						};
+						reader.readAsText(file);
+					};
+					
+					document.querySelector('#load').onclick = function(e){
+						document.querySelector('#file').click();
+					};
+					
+					function download(content, fileName, contentType) {
+						let a = document.createElement('a');
+						a.href = URL.createObjectURL(new Blob([content], {type: contentType}));
+						a.download = fileName;
+						a.click();
+					};
+					
+					document.querySelector('#save').onclick = function(e){
+						let state = JSON.stringify(window.Calc.getState());
+						let title = document.querySelector('#title').value;
+						download(state, title+'.json', 'text/plain');
+					};
 				</script>
 			</body>
-			</html>
+		</html>		
 		`;
 		// panel.onDidDispose(() => ...);
 	});
